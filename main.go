02d1bda0962b10cd18fa27030c682df30ba6c255
John van Hienen <jvanhienen@gmail.com>
2022-10-14 21:34:24 +0200

    feat: save to kubeconfig file of your choice

--- main.go
+++ main.go
@@ -1,16 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"runtime"
 )
 
+var (
+	version    = "v0.1.0"
+	goVersion  = runtime.Version()
+	versionStr = fmt.Sprintf("Akscreds version %v\n%v", version, goVersion)
+)
+
+type Config struct {
+	kubeConfigLocation string
+}
+
+func (c *Config) fillDefaults() {
+	c.kubeConfigLocation = fmt.Sprintf("%s/.kube/config", os.Getenv("HOME"))
+}
+
 func main() {
-	//err := login()
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
+	// TODO: Maybe rework all the shell commands to talk via the sdk. Removes the dependency on the 'az' binary.
+	config := Config{}
+	config.fillDefaults()
+
+	versionFlag := flag.Bool("v", false, "Displays the version number of Akscreds and Go.")
+	kubeConfigLocationOpt := flag.String("f", config.kubeConfigLocation, "Kubeconfig file to update.")
+	flag.Parse()
+	if *versionFlag {
+		fmt.Println(versionStr)
+		os.Exit(0)
+	}
+
+	err := login()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	subscriptionNames, err := retrieveSubscriptionNames()
 	if err != nil {
 		log.Fatalln(err)
@@ -29,19 +59,29 @@ func main() {
 		}
 
 		for _, cluster := range clusters {
-			fmt.Println(cluster.Name)
-			fmt.Println(cluster.ResourceGroup)
+			saveKubeConfig(cluster.Name, cluster.ResourceGroup, *kubeConfigLocationOpt)
 		}
-
 	}
 }
 
 func login() error {
-	// Maybe rework to login via the azure sdk instead of relying on the azure cli.
 	cmd := exec.Command("az", "login")
 	err := cmd.Run()
 	if err != nil {
 		return fmt.Errorf("could not complete login, error: %s", err)
 	}
+	return nil
+}
+
+func saveKubeConfig(clusterName string, resourceGroup string, file string) error {
+	cmd := exec.Command("az", "aks", "get-credentials",
+		"--name", clusterName,
+		"--resource-group", resourceGroup,
+		"--file", file)
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("could not save the credentials to the kubeconfig file %s, error: %s", file, err)
+	}
+	fmt.Printf("Succesfully saved credentials for %s to %s\n", clusterName, file)
 	return nil
 }
