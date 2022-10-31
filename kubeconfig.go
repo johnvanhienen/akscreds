0b1e65ca220e4b7397e103bd89853d9b11fcf8c3
John van Hienen <jvanhienen@gmail.com>
2022-10-31 20:57:19 +0100

    feat: rewrite server endpoint url

--- kubeconfig.go
+++ kubeconfig.go
@@ -1,92 +1,99 @@
 package main
 
 import (
-	"fmt"
-	"gopkg.in/yaml.v3"
-	"io/ioutil"
-	"os/exec"
+    "fmt"
+    "gopkg.in/yaml.v3"
+    "io/ioutil"
+    "os/exec"
 )
 
 type Kubeconfig struct {
-	APIVersion string `yaml:"apiVersion"`
-	Clusters   []struct {
-		Cluster struct {
-			CertificateAuthorityData string `yaml:"certificate-authority-data"`
-			Server                   string `yaml:"server"`
-		} `yaml:"cluster"`
-		Name string `yaml:"name"`
-	} `yaml:"clusters"`
-	Contexts []struct {
-		Context struct {
-			Cluster string `yaml:"cluster"`
-			User    string `yaml:"user"`
-		} `yaml:"context"`
-		Name string `yaml:"name"`
-	} `yaml:"contexts"`
-	CurrentContext string `yaml:"current-context"`
-	Kind           string `yaml:"kind"`
-	Preferences    struct {
-	} `yaml:"preferences"`
-	Users []struct {
-		Name string `yaml:"name"`
-		User struct {
-			AuthProvider struct {
-				Config struct {
-					ApiserverID string `yaml:"apiserver-id"`
-					ClientID    string `yaml:"client-id"`
-					ConfigMode  string `yaml:"config-mode"`
-					Environment string `yaml:"environment"`
-					TenantID    string `yaml:"tenant-id"`
-				} `yaml:"config"`
-				Name string `yaml:"name"`
-			} `yaml:"auth-provider"`
-		} `yaml:"user"`
-	} `yaml:"users"`
+    APIVersion string `yaml:"apiVersion"`
+    Clusters   []struct {
+        Cluster struct {
+            CertificateAuthorityData string `yaml:"certificate-authority-data"`
+            Server                   string `yaml:"server"`
+        } `yaml:"cluster"`
+        Name string `yaml:"name"`
+    } `yaml:"clusters"`
+    Contexts []struct {
+        Context struct {
+            Cluster string `yaml:"cluster"`
+            User    string `yaml:"user"`
+        } `yaml:"context"`
+        Name string `yaml:"name"`
+    } `yaml:"contexts"`
+    CurrentContext string `yaml:"current-context"`
+    Kind           string `yaml:"kind"`
+    Preferences    struct {
+    } `yaml:"preferences"`
+    Users []struct {
+        Name string `yaml:"name"`
+        User struct {
+            AuthProvider struct {
+                Config struct {
+                    ApiserverID string `yaml:"apiserver-id"`
+                    ClientID    string `yaml:"client-id"`
+                    ConfigMode  string `yaml:"config-mode"`
+                    Environment string `yaml:"environment"`
+                    TenantID    string `yaml:"tenant-id"`
+                } `yaml:"config"`
+                Name string `yaml:"name"`
+            } `yaml:"auth-provider"`
+        } `yaml:"user"`
+    } `yaml:"users"`
 }
 
 func saveKubeConfig(clusterName string, resourceGroup string, file string) error {
-	cmd := exec.Command("az", "aks", "get-credentials",
-		"--name", clusterName,
-		"--resource-group", resourceGroup,
-		"--file", file)
-	err := cmd.Run()
-	if err != nil {
-		return fmt.Errorf("could not save the credentials to the kubeconfig file %s, error: %s", file, err)
-	}
+    cmd := exec.Command("az", "aks", "get-credentials",
+        "--name", clusterName,
+        "--resource-group", resourceGroup,
+        "--file", file)
+    err := cmd.Run()
+    if err != nil {
+        return fmt.Errorf("could not save the credentials to the kubeconfig file %s, error: %s", file, err)
+    }
 
-	fmt.Printf("Succesfully saved credentials for %s to %s\n", clusterName, file)
-	return nil
+    fmt.Printf("Succesfully saved credentials for %s to %s\n", clusterName, file)
+    return nil
 }
 
-func rewriteClusterEndpoint(clusterName string, kubeconfigLocation string) error {
-	kubeconfig := Kubeconfig{}
-	kubeconfig, err := readKubeConfig(kubeconfigLocation)
-	if err != nil {
-		return fmt.Errorf("could not read kubeconfig file, error: %s", err)
-	}
-	for _, cluster := range kubeconfig.Clusters {
-		cluster.Cluster.Server = "hoi"
-	}
+func rewriteClusterEndpoint(clusterName string, kubeconfigLocation string, newServerEndpoint string) error {
+    kubeConfig := Kubeconfig{}
+    kubeConfig, err := readKubeConfig(kubeconfigLocation)
+    if err != nil {
+        return fmt.Errorf("could not read kubeConfig file, error: %s", err)
+    }
 
-	d, err := yaml.Marshal(&kubeconfig)
-	if err != nil {
-		fmt.Errorf("error: %v", err)
-	}
-	fmt.Printf("--- t dump:\n%s\n\n", string(d))
-	return nil
+    for i, cluster := range kubeConfig.Clusters {
+        if cluster.Name == clusterName {
+            kubeConfig.Clusters[i].Cluster.Server = newServerEndpoint
+        }
+    }
+    d, err := yaml.Marshal(&kubeConfig)
+    if err != nil {
+        fmt.Errorf("error: %v", err)
+    }
+
+    err = ioutil.WriteFile(kubeconfigLocation, d, 0644)
+    if err != nil {
+        return fmt.Errorf("could not write to kubeConfig file, error: %s", err)
+    }
+
+    return nil
 }
 
 func readKubeConfig(file string) (Kubeconfig, error) {
-	kubeconfig := Kubeconfig{}
-	data, err := ioutil.ReadFile(file)
-	if err != nil {
-		return Kubeconfig{}, fmt.Errorf("could not read kubeconfig file, error: %s", err)
-	}
+    kubeconfig := Kubeconfig{}
+    data, err := ioutil.ReadFile(file)
+    if err != nil {
+        return Kubeconfig{}, fmt.Errorf("could not read kubeconfig file, error: %s", err)
+    }
 
-	err = yaml.Unmarshal(data, &kubeconfig)
-	if err != nil {
-		return Kubeconfig{}, fmt.Errorf("could not parse yaml from kubeconfig, error: %s", err)
-	}
+    err = yaml.Unmarshal(data, &kubeconfig)
+    if err != nil {
+        return Kubeconfig{}, fmt.Errorf("could not parse yaml from kubeconfig, error: %s", err)
+    }
 
-	return kubeconfig, nil
+    return kubeconfig, nil
 }
--- main.go
+++ main.go
@@ -1,78 +1,77 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"log"
-	"os"
-	"runtime"
+    "flag"
+    "fmt"
+    "log"
+    "os"
+    "runtime"
 )
 
 var (
-	version    = "v0.1.0"
-	goVersion  = runtime.Version()
-	versionStr = fmt.Sprintf("Akscreds version %v\n%v", version, goVersion)
+    version    = "v0.1.0"
+    goVersion  = runtime.Version()
+    versionStr = fmt.Sprintf("Akscreds version %v\n%v", version, goVersion)
 )
 
 type Config struct {
-	kubeConfigLocation string
+    kubeConfigLocation string
 }
 
 func (c *Config) fillDefaults() {
-	c.kubeConfigLocation = fmt.Sprintf("%s/.kube/config", os.Getenv("HOME"))
+    c.kubeConfigLocation = fmt.Sprintf("%s/.kube/config", os.Getenv("HOME"))
 }
 
 func main() {
-	// TODO: Maybe rework all the shell commands to talk via the sdk. Removes the dependency on the 'az' binary.
-	config := Config{}
-	config.fillDefaults()
+    // TODO: Maybe rework all the shell commands to talk via the sdk. Removes the dependency on the 'az' binary.
+    config := Config{}
+    config.fillDefaults()
 
-	versionFlag := flag.Bool("v", false, "Displays the version number of Akscreds and Go.")
-	allTenantFlag := flag.Bool("A", false, "Retrieve credentials from all tenants which are shown in 'az account list'")
-	rewriteServerEndpointOpt := flag.String("r", "", "Rewrite server endpoint in from saved config to specified value. ")
-	kubeConfigLocationOpt := flag.String("f", config.kubeConfigLocation, "Kubeconfig file to update.")
-	flag.Parse()
-	if *versionFlag {
-		fmt.Println(versionStr)
-		os.Exit(0)
-	}
+    versionFlag := flag.Bool("v", false, "Displays the version number of Akscreds and Go.")
+    allTenantFlag := flag.Bool("A", false, "Retrieve credentials from all tenants which are shown in 'az account list'")
+    rewriteServerEndpointOpt := flag.String("r", "", "Rewrite server endpoint in from saved config to specified value. ")
+    kubeConfigLocationOpt := flag.String("f", config.kubeConfigLocation, "Kubeconfig file to update.")
+    flag.Parse()
+    if *versionFlag {
+        fmt.Println(versionStr)
+        os.Exit(0)
+    }
 
-	loggedInTenantId, err := loginAccount()
-	if err != nil {
-		log.Fatalln(err)
-	}
+    loggedInTenantId, err := loginAccount()
+    if err != nil {
+        log.Fatalln(err)
+    }
 
-	subscriptions := Subscriptions{}
-	subscriptions, err = retrieveAllSubscriptions()
-	if err != nil {
-		log.Fatalln(err)
-	}
+    subscriptions := Subscriptions{}
+    subscriptions, err = retrieveAllSubscriptions()
+    if err != nil {
+        log.Fatalln(err)
+    }
 
-	var subscriptionNames []string
-	if *allTenantFlag {
-		subscriptionNames = subscriptions.getAllSubscriptionNames()
-	} else {
-		subscriptionNames = subscriptions.getAllSubscriptionNamesByTenantIds([]string{loggedInTenantId})
-	}
+    var subscriptionNames []string
+    if *allTenantFlag {
+        subscriptionNames = subscriptions.getAllSubscriptionNames()
+    } else {
+        subscriptionNames = subscriptions.getAllSubscriptionNamesByTenantIds([]string{loggedInTenantId})
+    }
 
-	for _, subscriptionName := range subscriptionNames {
-		err := setActiveSubscription(subscriptionName)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		fmt.Printf("Setting subscription name to: \"%s\"\n", subscriptionName)
+    for _, subscriptionName := range subscriptionNames {
+        err := setActiveSubscription(subscriptionName)
+        if err != nil {
+            log.Fatalln(err)
+        }
+        fmt.Printf("Setting subscription name to: \"%s\"\n", subscriptionName)
 
-		clusters, err := retrieveClusters()
-		if err != nil {
-			log.Println(err)
-		}
+        clusters, err := retrieveClusters()
+        if err != nil {
+            log.Println(err)
+        }
 
-		for _, cluster := range clusters {
-			saveKubeConfig(cluster.Name, cluster.ResourceGroup, *kubeConfigLocationOpt)
-			if *rewriteServerEndpointOpt != "" {
-				rewriteClusterEndpoint(cluster.Name, *kubeConfigLocationOpt)
-			}
-			os.Exit(0)
-		}
-	}
+        for _, cluster := range clusters {
+            saveKubeConfig(cluster.Name, cluster.ResourceGroup, *kubeConfigLocationOpt)
+            if *rewriteServerEndpointOpt != "" {
+                rewriteClusterEndpoint(cluster.Name, *kubeConfigLocationOpt, *rewriteServerEndpointOpt)
+            }
+        }
+    }
 }
